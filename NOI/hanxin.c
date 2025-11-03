2b68e7851540cec50db6721721e1bc72332c3d0f
omarfu8424 <furuer8424@163.com>
2025-11-03 22:41:26 +0800

    更新中文

--- NOI/hanxin.c
+++ NOI/hanxin.c
@@ -8,9 +8,9 @@ int main()
     {
         if(i<10) continue;
         if(i>100) break;
-        if(i%3==a&&i%5==b&&i%7==c) {printf("%d\n",i);break;}
+        if(i%3==a&&i%5==b&&i%7==c) {printf("сп%d\n",i);break;}
         if(i==100)
-        printf("no answer");
+        printf("нч");
         
     }
     return 0;
