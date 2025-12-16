64bd544147876c1f446f7a05de043633211e5d22
omarfu8424 <furuer8424@163.com>
2025-12-16 20:19:07 +0800

    fix

--- luogu/U400106.c
+++ luogu/U400106.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
-#define MAX 300
+#define MAX 100000
 
 int main(){
-    int a[MAX]={0,0};
+    int a[MAX]={0};
     int b[MAX]={0};
 
     int t;
@@ -11,41 +11,44 @@ int main(){
     scanf("%d %d",&num,&t);
 
     a[0]=num;
+    a[1]=0;
 
     int times=t;
+    if(t==1)    printf("%d",num);
+    else{
+        while(times--){
 
-    while(times--){
+            int* write = b;
+            int* read = a;
 
-        int* write = b;
-        int* read = a;
+            int _count,_num;
+            _count=_num=0;
 
-        int _count,_num;
-        _count=_num=0;
+            while(*read!=0){
+                _num=*read;
+                _count=0;
 
-        while(*read!=0){
-            _num=*read;
-            _count=0;
+                while(*read==_num){
+                    read++;
+                    _count++;
+                }
 
-            while(*read==_num){
-                read++;
-                _count++;
+                *(write++)=_count;
+                *(write++)=_num;
             }
 
-            *(write++)=_count;
-            *(write++)=_num;
+            int lenth = write - b;
+
+            for(int i=0;i<lenth;i++){
+                a[i]=b[i];
+            }       
+            a[lenth]=0;
+
+
+            if(times==1)for(int i=0;i<lenth;i++){
+                printf("%d",b[i]);
+            } 
         }
-
-        int lenth = write - b;
-
-        for(int i=0;i<lenth;i++){
-            a[i]=b[i];
-        }       
-        a[lenth]=0;
-
-
-        if(times==1)for(int i=0;i<lenth;i++){
-            printf("%d",b[i]);
-        } 
     }
 
     return 0;
